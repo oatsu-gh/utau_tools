8c4008fdb960e13c74d2d8ccedd96b31f8b8b85a
oatsu <panchi.psp@gmail.com>
2024-03-20 14:54:18 +0900

    created ust2csv

---
+++ ust2csv/ust2csv.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 oatsu
+"""
+USTファイルのノート部分の情報をCSVに変換する。
+ファイル名も記録する。
+"""
+from glob import glob
+from os.path import basename, isdir, join
+
+import pandas
+import utaupy
+
+
+def note2dict(note):
+    """
+    utaupy.ust.Note を 通常のdictに変換する。
+    このとき、テンポを確実に埋め込む。
+    """
+    d = note.data
+    d['Tempo'] = note.tempo
+    d['TimeSignatures'] = note.timesignatures
+    return d
+
+
+def ust2df(path_ust_in):
+    """
+    USTファイルを指定して、CSVファイルに変換して出力する。
+    """
+    # ustファイルを読み取る
+    ust = utaupy.ust.load(path_ust_in)
+    # ustに辞書のリストに変換する
+    l = list(map(note2dict, ust.notes))
+    # 辞書のリストをdataframeに変換する
+    df = pandas.json_normalize(l)
+    # USTファイル名のカラムを追加する
+    df['FileName'] = basename(path_ust_in)
+
+    return df
+
+
+def main():
+    """
+    フォルダかファイルを指定して一括変換する。
+    フォルダを指定した場合は複数のUSTを単一ファイルにまとめて結果とする。
+    """
+    # 各種パスを指定
+    ust_dir = input('Select a directory or a UST file: ').strip('"')
+    ust_files = glob(join(ust_dir, '*.ust')) if isdir(ust_dir) else [ust_dir]
+    # USTを一括処理して単一のDataFrameにする
+    df_concat = pandas.concat(map(ust2df, ust_files))
+    # CSV出力
+    df_concat.to_csv('./result_utf8.csv', encoding='utf-8')
+    df_concat.to_csv('./result_sjis.csv', encoding='cp932')
+
+
+if __name__ == '__main__':
+    main()
